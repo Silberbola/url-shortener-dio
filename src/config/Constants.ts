470808a0ae4e689a4565bfcf0f880a2d20814631
Alexandre Silberstein <84195049+Silberbola@users.noreply.github.com>
2022-07-18 18:56:19 +0200

    Atualizando BD

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
 	API_URL: 'http://localhost:5000',
 	MONGO_CONNECTION:
-		'mongodb+srv://<user>:<password>@<database>.yptlq.mongodb.net/url-shortener-dio?retryWrites=true&w=majority',
+		'mongodb+srv://Silberbola:<password>@url-shotener-dio.i8xlhdg.mongodb.net/?retryWrites=true&w=majority',
 }
